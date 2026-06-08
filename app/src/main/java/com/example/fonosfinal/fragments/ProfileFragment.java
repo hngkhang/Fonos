7e40258edadb41254c52d5a94a82a2df17344539
dung17112005 <152056219+dung17112005@users.noreply.github.com>
2026-06-09 02:50:31 +0700

    Done Firebase connection + login + signup

--- app/src/main/java/com/example/fonosfinal/fragments/ProfileFragment.java
+++ app/src/main/java/com/example/fonosfinal/fragments/ProfileFragment.java
@@ -1,5 +1,6 @@
 package com.example.fonosfinal.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,6 +10,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.example.fonosfinal.LoginActivity;
 import com.example.fonosfinal.R;
 
 public class ProfileFragment extends Fragment {
@@ -17,5 +19,16 @@ public class ProfileFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_profile, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        view.findViewById(R.id.button_logout).setOnClickListener(v -> {
+            Intent intent = new Intent(requireContext(), LoginActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
+        });
     }
 }
